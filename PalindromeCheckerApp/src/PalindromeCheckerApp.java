e1dfcfc32aac4a39832cad2dc127acfae1b37851
MDVirinchi <rushi18126@gmail.com>
2026-02-18 16:35:06 +0530

    print a hardcoded palindrome values

---
+++ PalindromeCheckerApp/src/PalindromeCheckerApp.java
@@ -0,0 +1,22 @@
+public class PalindromeCheckerApp {
+
+        public static void main(String[] args) {
+
+            String input = "madam"; // hardcoded string
+            boolean isPalindrome = true;
+
+            // check till half length
+            for (int i = 0; i < input.length() / 2; i++) {
+                if (input.charAt(i) != input.charAt(input.length() - 1 - i)) {
+                    isPalindrome = false;
+                    break;
+                }
+            }
+
+            if (isPalindrome) {
+                System.out.println(input + " is a palindrome");
+            } else {
+                System.out.println(input + " is not a palindrome");
+            }
+        }
+    }
