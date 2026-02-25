a40e6aa45a4b86887b58e61e8b8e5a7b7eb6a21b
MDVirinchi <rushi18126@gmail.com>
2026-02-25 15:02:04 +0530

    Virinchi UC4

--- PalindromeCheckerApp/src/PalindromeCheckerApp.java
+++ PalindromeCheckerApp/src/PalindromeCheckerApp.java
@@ -4,20 +4,15 @@ public class PalindromeCheckerApp {
 
     public static void main(String[] args) {
 
-        // Declare input string
         String input = "radar";
 
-        // Convert string to character array
         char[] chars = input.toCharArray();
 
-        // Initialize pointers
         int start = 0;
         int end = chars.length - 1;
 
-        // Assume palindrome
         boolean isPalindrome = true;
 
-        // Compare characters
         while (start < end) {
             if (chars[start] != chars[end]) {
                 isPalindrome = false;
